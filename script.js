c254dcf87ef403f3ac14a95a8c13eab55a171495
mutednew <artehaguk@gmail.com>
2024-12-08 00:18:18 +0200

    first

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 /* Intro 
 ================================*/
-const intro = document.querySelector('.intro'),
-      introHeight = intro.offsetHeight,
-      scrollBtn = document.querySelector('.project-btn');
+const activeScroll = document.querySelectorAll('.active-scroll'),
+    introH = activeScroll[0].offsetHeight,
+    btn = document.querySelector('.project-btn');
+let autoScrolling = false;
 
-scrollBtn.addEventListener('click', () => {
-    window.scrollTo( {
-        top: introHeight,
-        behavior: 'smooth'
-    });
+function handleScroll() {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll < introH && !autoScrolling) {
+        autoScrolling = true;
+
+        window.scrollTo({
+            top: introH,
+            behavior: 'smooth'
+        });
+
+        setTimeout(() => {
+            autoScrolling = false;
+        }, 1000);
+    }
+}
+
+function enableFreeScroll() {
+    document.body.style.overflow = 'auto';
+}
+
+window.addEventListener('scroll', handleScroll);
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= introH) {
+        enableFreeScroll();
+    }
 });
 
+function sectionEffect() {
+    activeScroll.forEach((el) => {
+        const rect = el.getBoundingClientRect();
 
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+            el.classList.remove('hidden');
+        } else {
+            el.classList.add('hidden');
+        }
+    });
+}
+
+window.addEventListener('scroll', sectionEffect);
+
+document.addEventListener('DOMContentLoaded', () => {
+    activeScroll.forEach((el, i) => {
+        if (i > 0) {
+            el.classList.add('hidden');
+        }
+    });
+});
 
 /**
  * LINK
